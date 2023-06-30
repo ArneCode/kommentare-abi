6b92253792323bbc4f5704165de48fbfbabe75f7
SuffelDev <108270169+SuffelDev@users.noreply.github.com>
2023-06-30 12:24:30 +0000

    Update script.js

--- script.js
+++ script.js
@@ -22,9 +22,9 @@ fetch('kommentare.json')
     
     console.log("kommentare: ", comments);
     //Korrektur zu Giustinas Jahrbuch-Zitat weil ich das falsch abetippt hab und sie sich eine richtigstellung wünscht
-    if(id==30){ //Ihre ID
+    if(id==31){ //Ihre ID
       console.log("Das ist die richtige Seite")
-      document.getElementByID("giustinaKorrektur").removeAttribute("hidden")
+      document.getElementByID("giustinaKorrektur").setAttribute("hidden",false)
     }
 
     // Get the element with the id "Kommentare"
